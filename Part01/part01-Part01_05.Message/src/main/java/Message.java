1b0a2608ac1542944d5c146d1527c647d95a713d
medeniz <medeniz@gmail.com>
2021-10-17 11:32:58 +0200

    code added for part01_05

--- Part01/part01-Part01_05.Message/src/main/java/Message.java
+++ Part01/part01-Part01_05.Message/src/main/java/Message.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class Message {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Write a message:");
         String message = scanner.nextLine();
         System.out.println(message);
