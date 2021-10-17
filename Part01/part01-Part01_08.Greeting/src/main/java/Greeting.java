a369112341f7dd6dde250be316ad46ced0f02eff
medeniz <medeniz@gmail.com>
2021-10-17 11:30:59 +0200

    added my solutions for part01_08

--- Part01/part01-Part01_08.Greeting/src/main/java/Greeting.java
+++ Part01/part01-Part01_08.Greeting/src/main/java/Greeting.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class Greeting {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+        Scanner scanner = new Scanner(System.in);
         System.out.println("What's your name?");
         String name = scanner.nextLine();
         System.out.println("Hi "+ name );
