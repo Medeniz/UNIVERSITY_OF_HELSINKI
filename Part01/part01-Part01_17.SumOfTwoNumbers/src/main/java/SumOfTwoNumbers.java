9f124ebf97741a1c5879777141d22a8edc972b49
medeniz <medeniz@gmail.com>
2021-10-19 12:33:10 +0200

    added my solutions for part01_17

--- Part01/part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
+++ Part01/part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class SumOfTwoNumbers {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give the first number:");
         int first = Integer.valueOf(scanner.nextLine());
