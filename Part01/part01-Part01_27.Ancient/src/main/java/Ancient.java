5fc5ac3140101f66308191eb72859541e4704a40
medeniz <medeniz@gmail.com>
2021-10-21 22:40:29 +0200

    added my solutions for part01_27

--- Part01/part01-Part01_27.Ancient/src/main/java/Ancient.java
+++ Part01/part01-Part01_27.Ancient/src/main/java/Ancient.java
@@ -5,7 +5,6 @@ public class Ancient {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         System.out.println("Give a number:");
         int num = Integer.valueOf(scan.nextLine());
         if (num < 2015) {
