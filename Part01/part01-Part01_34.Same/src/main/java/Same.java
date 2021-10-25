a7adce75e2be036c9b51b387c98ff215483fc345
medeniz <medeniz@gmail.com>
2021-10-25 22:43:13 +0200

    added my solutions for part01_33

--- Part01/part01-Part01_34.Same/src/main/java/Same.java
+++ Part01/part01-Part01_34.Same/src/main/java/Same.java
@@ -6,14 +6,14 @@ public class Same {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-         System.out.println("Enter the first string:");
+        System.out.println("Enter the first string:");
         String str1 = scan.nextLine();
-         System.out.println("Enter the second string:");
-        String str2= scan.nextLine();
-        if(str1.equals(str2)){
+        System.out.println("Enter the second string:");
+        String str2 = scan.nextLine();
+        if (str1.equals(str2)) {
             System.out.println("Same");
-        }else{
+        } else {
             System.out.println("Different");
+        }
     }
-}
 }
