e13b8ba6d16e48581bc8fe7ef42f4019d300cdce
medeniz <medeniz@gmail.com>
2021-10-25 22:34:16 +0200

    added my solutions for part01_31

---
+++ Part01/part01-Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class GradesAndPoints {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Give points [0-100]:");
+        int num = Integer.valueOf(scan.nextLine());
+
+        if (num > 100) {
+            System.out.println("Grade:incredible!");
+        } else if (num >= 90) {
+            System.out.println("Grade:i5");
+        } else if (num >= 80) {
+            System.out.println("Grade:i4");
+        } else if (num >= 70) {
+            System.out.println("Grade:i3");
+        } else if (num >= 60) {
+            System.out.println("Grade:i2");
+        } else if (num >= 50) {
+            System.out.println("Grade:i1");
+        } else if (num >= 0) {
+            System.out.println("Grade:ifailed");
+        }else {
+            System.out.println("Grade:iimpossible!");
+        }
+
+    }
+}
