fa19eb2946f5bdcbbfad86787361a917e10a78f2
medeniz <medeniz@gmail.com>
2021-10-26 22:28:29 +0200

    added my solutions for part02_03

--- Part02/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ Part02/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class AbsoluteValue {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int num = Integer.valueOf(scanner.nextLine());
         if (num > 0) {
