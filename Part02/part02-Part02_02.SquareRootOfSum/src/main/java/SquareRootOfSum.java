6a69c0122337364318828b012506fff08de72440
medeniz <medeniz@gmail.com>
2021-10-26 22:27:18 +0200

    added my solutions for part02_02

--- Part02/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ Part02/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class SquareRootOfSum {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int num1 = Integer.valueOf(scanner.nextLine());
 
