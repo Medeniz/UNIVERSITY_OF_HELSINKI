ac93ec9bef4a4aeb088d46530391ee5f47acb81f
medeniz <medeniz@gmail.com>
2021-10-29 21:19:42 +0200

    added my solutions for part02_08

--- Part02/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ Part02/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -1,26 +1,22 @@
-
 import java.util.Scanner;
 
 public class NumberOfNumbers {
 
     public static void main(String[] args) {
-        //number of numbers
+
         int counter = 0;
         Scanner scan = new Scanner(System.in);
-
 
         while (true) {
             System.out.println("Give a number:");
             int num = Integer.valueOf(scan.nextLine());
-            if (num!=0){
-                counter++;               
-                
-            }else{
+            if (num != 0) {
+                counter++;
+
+            } else {
                 System.out.println("Number of numbers: " + counter);
                 break;
             }
-
         }
-
     }
 }
