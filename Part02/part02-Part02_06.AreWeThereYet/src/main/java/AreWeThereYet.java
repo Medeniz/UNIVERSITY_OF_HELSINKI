0c1cfd459c0b72392b1b4c854ffc4c3033aa835a
medeniz <medeniz@gmail.com>
2021-10-29 21:12:22 +0200

    added my solutions for part02_06

--- Part02/part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
+++ Part02/part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
@@ -1,19 +1,16 @@
-
 import java.util.Scanner;
 
 public class AreWeThereYet {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while (true){
-   System.out.println("Give a number:");
-        int number=Integer.valueOf(scanner.nextLine());
-       
-        if (number==4){
-            break;
-        }
-       
-    }
 
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == 4) {
+                break;
+            }
+        }
     }
 }
