0199ea9fe3e8c49cf6a444747a92d53ff2eb4b06
medeniz <medeniz@gmail.com>
2021-10-29 21:22:36 +0200

    added my solutions for part02_09

---
+++ Part02/part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class NumberOfNegativeNumbers {
+
+    public static void main(String[] args) {
+        //number of negative numbers
+        int counter = 0;
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scan.nextLine());
+            if (num > 0) {
+
+            } else if (num < 0) {
+                counter++;
+            } else {
+
+                System.out.println("Number of negative numbers: " + counter);
+                break;
+            }
+
+
+        }
+    }
+
+}
+
